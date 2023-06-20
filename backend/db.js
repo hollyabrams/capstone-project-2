61f844cffac53304291c26705c749339d63c97c4
Holly <holly.d.abrams@gmail.com>
2023-06-20 12:46:24 -0400

    Fix: Update production database connection string

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    connectionString: `postgres://localhost:5432/${getDatabaseUri()}`,
+    connectionString: getDatabaseUri(),
     ssl: {
       rejectUnauthorized: false
     }
